43d2e3ed39921fb42ad49d6e8b7ec2c2c23678c8
DriSidibe <dsidibe653@gmail.com>
2022-12-14 17:55:26 +0000

    commit

--- includes/opcode_handdler.h
+++ includes/opcode_handdler.h
@@ -1,5 +1,7 @@
 #ifndef __OPCODE_HANDDLER__
 #define __OPCODE_HANDDLER__
 
+stack_t push(stack_t **stack, unsigned int line_number);i
+void pall(stack_t **stack, __attribute__((unused)) unsigned int line_number);
 
 #endif
---
+++ includes/test_args.h
@@ -0,0 +1,5 @@
+#ifndef __TEST_ARGS__
+#define __TEST_ARGS__
+
+
+#endif
